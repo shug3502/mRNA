1ae783444fa4135d24277543eb09c2cd79e0601b
Jonathan Harrison <harrison@maths.ox.ac.uk>
2015-05-05 16:59:29 +0100

    Calculates simulation and analytical results for second spatial moment

--- run_and_calculate_mean_position.m
+++ run_and_calculate_mean_position.m
@@ -9,6 +9,7 @@ L=30; %length of cell
 delx = 1; %bin size for integral
 
 mean_position = zeros(size(time_vec));
+second_moment = zeros(size(time_vec));
 q_estimate = zeros(L+1,length(time_vec));
 
 for k = 1:length(time_vec)
@@ -26,24 +27,71 @@ for k = 1:length(time_vec)
     [Num_in_bins,edges] = histc(final_xpos,0:delx:L);
     q_estimate(:,k) = Num_in_bins/N; %estimate of q at time T
     mean_position(k) = (delx/2:delx:(L-delx/2))*q_estimate(1:L,k)*delx+delx*L*q_estimate(L+1,k); %use centre of each bin 
+    second_moment(k) = (delx/2:delx:(L-delx/2)).^2*q_estimate(1:L,k)*delx+delx*L^2*q_estimate(L+1,k); %use centre of each bin    
 end
+second_moment(1)
 speed_estimate = diff(mean_position)/dt
-sum(speed_estimate(1:8))/8
+%sum(speed_estimate(1:8))/8
 figure(2)
 subplot(2,1,1);
 plot(time_vec,mean_position,'r','linewidth',3)
-xlabel('t')
-ylabel('mean position')
 grid on
+hold on
+
+%PLOT ANALYTIC RESULT
+mu_initial = 0.5; %initial mean position
+nu_1 = 0.4; %speed in active transport mode
+mu1 = min((nu_1*(4*phi/100-2)/pi*time_vec*60^2 + mu_initial),L); %note time scaled to seconds
+if num_modes>1
+    mu1 = min((0.5*nu_1*(4*phi/100-2)/pi*time_vec*60^2 + mu_initial),L); %adjust for different analytical results for different numbers of modes
+end
+plot(time_vec, mu1, 'g--', 'linewidth',3);
+set(gca, 'fontsize',14)
+xlabel('t');
+ylabel('mean position');
+
 for s=1:length(time_vec)
 subplot(2,1,2);
 bar(0:delx:L,q_estimate(:,s))
+set(gca, 'fontsize',14)
 xlabel('x')
 ylabel('q')
 grid on
 axis([0,35,0,1])
 pause(0.4)
 end
+
+figure(3)
+plot(time_vec,second_moment,'r','linewidth',3)
+grid on
+hold on
+
+%PLOT ANALYTIC RESULT
+mu_initial = 0.5; %initial mean position
+nu_1 = 0.4; %speed in active transport mode
+omega = 1/6; %switching rate
+mu2 = min((nu_1*(4*phi/100-2)/pi*time_vec*60^2).^2 + 2*nu_1*(4*phi/100-2)/pi*time_vec*60^2*mu_initial+ mu_initial^2,L^2); %note time scaled to seconds
+if num_modes>1
+   %adjust for different analytical results for different numbers of modes
+% mu2_prime = min(mu_initial^2 + nu_1/2*(4*phi/100-2)/pi*( 2*mu_initial*time_vec*60^2 ... 
+%     + nu_1/2*(4*phi/100-2)/pi*(time_vec*60^2).^2 ...
+%      + mu_initial/(2*omega)*exp(2*omega*time_vec*60^2) ... 
+%      + nu_1*(4*phi/100-2)/pi*time_vec*60^2.*exp(2*omega*time_vec*60^2)/(2*omega) ...
+%      - mu_initial/(2*omega) - nu_1*(4*phi/100-2)/pi*exp(2*omega*time_vec*60^2)/(4*omega^2)...
+%      + nu_1/(4*omega^2)*(4*phi/100-2)/pi),L^2);
+
+mu2 = min(mu_initial^2 + nu_1/2*(4*phi/100-2)/pi*( 2*mu_initial*time_vec*60^2 ... 
+   + nu_1/2*(4*phi/100-2)/pi*(time_vec*60^2).^2),L^2);
+
+end
+%plot(time_vec, mu2_prime, 'k--', 'linewidth',3);
+plot(time_vec, mu2, 'g-.', 'linewidth',3);
+set(gca, 'fontsize',14)
+xlabel('t');
+ylabel('second moment');
+
+second_moment
+mu2
 toc;
 end
 
