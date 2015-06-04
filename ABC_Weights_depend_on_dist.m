bf298780e50170d8fce5f064ffd036100b547fd7
Jonathan Harrison <harrison@maths.ox.ac.uk>
2015-06-04 17:12:25 +0100

    Can now choose which summary statistics to use via an option option_a

--- ABC_Weights_depend_on_dist.m
+++ ABC_Weights_depend_on_dist.m
@@ -19,31 +19,32 @@ rng(my_seed);
 close all
 
 %Fake parameters
-    params.nu1 = 1.0; %speed of RNP complex under active transport [zimyanin et al 2008]
-    params.nu2 = 0.80; %ratio between speed for active transport vs diffusion [zimyanin et al 2008]
-    params.lambda_1=0;   %1/0.13; %transition rate =7.69 [zimyanin et al 2008]
-    params.lambda_2 = 0.11;
-    params.omega_1= 0.42;    %1/6*(num_modes>1); %rate of falling off the microtubule [zimyanin et al 2008] since average track length 2.4 - 2.8 microns -> average jump for 6s -> rate 1/6
-    params.omega_2 = 0.84;
-    params.phi = 0.58; %percentage of microtubules in posterior direction for biased angle distn [parton et al 2011]
-    params.x_0=0.5;  %Initially in first compartment, ie. at NPC
-    params.Lx = 52; %length of cell in x direction
-    params.Ly = 37; %in y direction
-    params.nuc_radius = 10; %radius of nucleus
-    params.theta_0 = 0; %initial angle is 0
+params.nu1 = 1.0; %speed of RNP complex under active transport [zimyanin et al 2008]
+params.nu2 = 0.80; %ratio between speed for active transport vs diffusion [zimyanin et al 2008]
+params.lambda_1=0;   %1/0.13; %transition rate =7.69 [zimyanin et al 2008]
+params.lambda_2 = 0.11;
+params.omega_1= 0.42;    %1/6*(num_modes>1); %rate of falling off the microtubule [zimyanin et al 2008] since average track length 2.4 - 2.8 microns -> average jump for 6s -> rate 1/6
+params.omega_2 = 0.84;
+params.phi = 0.58; %percentage of microtubules in posterior direction for biased angle distn [parton et al 2011]
+params.x_0=0.5;  %Initially in first compartment, ie. at NPC
+params.Lx = 52; %length of cell in x direction
+params.Ly = 37; %in y direction
+params.nuc_radius = 10; %radius of nucleus
+params.theta_0 = 0; %initial angle is 0
 
 real_params = [params.nu1, params.nu2, params.lambda_2, params.omega_1, params.omega_2, params.phi, params.x_0, params.Lx, params.Ly, params.nuc_radius, params.theta_0];
-
-%Generate fake data
-%calculate appropriate summary statistic - we choose MFPT
-q_estimate_fake = summary_statistic_calculator(params,1000,0)
 
 %Choose tolerance sequence
 accepted_proportion = 0.5; %alpha
 %At t=1 for first generation
 %N=500;
 
-p_accept_min = 0.2; % 1%
+p_accept_min = 2; %0.1; 
+option_a = 1; %1 gives euclidean distance and mfpt etc. 0 gives spatial distribution and kl div etc.
+
+%Generate fake data
+%calculate appropriate summary statistic - we choose MFPT
+q_estimate_fake = summary_statistic_calculator(params,1000,0,option_a)
 
 %create while loop
 
@@ -68,8 +69,8 @@ for i=1:N
     
     %simulate data using these model parameters
     %Calculate summary statistic (MFPT)
-    q_estimate_candidate = summary_statistic_calculator(par_params,20,1);
-    abc_dist(i) = distance_metric(q_estimate_candidate,q_estimate_fake,params.Lx); %distance of proposed S(x) from S(x_obs)
+    q_estimate_candidate = summary_statistic_calculator(par_params,20,1,option_a);
+    abc_dist(i) = distance_metric(q_estimate_candidate,q_estimate_fake,params.Lx,option_a); %distance of proposed S(x) from S(x_obs)
     %end    %repeat until N acceptances have been made
     
     abc_theta(i,:) = par_params(p_indices);
@@ -153,8 +154,8 @@ while p_accept >p_accept_min
         
         %simulate data using these model parameters
         %Calculate summary statistic (MFPT)
-        q_estimate_candidate = summary_statistic_calculator(par_params(my_index,:),20,1);
-        abc_dist(i) = distance_metric(q_estimate_candidate,q_estimate_fake,params.Lx);
+        q_estimate_candidate = summary_statistic_calculator(par_params(my_index,:),20,1,option_a);
+        abc_dist(i) = distance_metric(q_estimate_candidate,q_estimate_fake,params.Lx,option_a);
         
         
         %end    %repeat until N acceptances have been made
@@ -175,7 +176,7 @@ while p_accept >p_accept_min
     end
     to_keep = (abc_dist <= quantile(abc_dist,accepted_proportion));
     abc_theta = abc_theta(to_keep,:);
-    abc_weights = abc_weights(to_keep)./sum(abc_weights(to_keep));  %choose not to renormalise 
+    abc_weights = abc_weights(to_keep)./sum(abc_weights(to_keep));  %choose not to renormalise
     abc_dist = abc_dist(to_keep);
     ma = max(abc_dist);
     mi = min(abc_dist);
@@ -187,7 +188,7 @@ while p_accept >p_accept_min
     end
     sigma = 2*wvariance;  %var(abc_theta.*repmat(abc_weights,1,3)); %weighted set of theta values
     %sigma_alt = 2*var(abc_theta);
-    p_accept = 1/(N-N_current)*sum(to_keep((N_current+1):N)) 
+    p_accept = 1/(N-N_current)*sum(to_keep((N_current+1):N))
     entropy = calculate_entropy(abc_theta,prior_params,prior_sigma,p_indices);
     
     figure(my_seed+1);
@@ -256,8 +257,8 @@ ylabel('param3');
 box = zeros(length(p_indices),2);
 contained_in_pred_interval = 1;
 for k=1:3
-box(k,:) = [quantile(abc_theta(:,k),0.025),quantile(abc_theta(:,k),1-0.025)];
-contained_in_pred_interval = contained_in_pred_interval*(real_params(p_indices(k))>box(k,1))*(real_params(p_indices(k))<box(k,2));
+    box(k,:) = [quantile(abc_theta(:,k),0.025),quantile(abc_theta(:,k),1-0.025)];
+    contained_in_pred_interval = contained_in_pred_interval*(real_params(p_indices(k))>box(k,1))*(real_params(p_indices(k))<box(k,2));
 end
 box
 
@@ -269,70 +270,136 @@ fclose('all');
 toc
 end
 
-function [q_estimate] = summary_statistic_calculator(par_params,N,is_parallel)
-%runs velocityjump2D_ModesInput which is a velocity jump process for a single particle
-%runs this many times for many particles and returns simulated estimates of
-%mfpt
+%use mfpt, num_jumps and jump distances as summary statistics
+function [q_estimate] = summary_statistic_calculator(par_params,num_particles,is_parallel,option_a_summary_statistic)
 
-delx = 1; %bin size;
-if is_parallel
-    params.nu1 = par_params(1); %speed of RNP complex under active transport [zimyanin et al 2008]
-    params.nu2 = par_params(2); %ratio between speed for active transport vs diffusion [zimyanin et al 2008]
-    params.lambda_1=0;   %1/0.13; %transition rate =7.69 [zimyanin et al 2008]
-    params.lambda_2 = par_params(3);
-    params.omega_1= par_params(4);    %1/6*(num_modes>1); %rate of falling off the microtubule [zimyanin et al 2008] since average track length 2.4 - 2.8 microns -> average jump for 6s -> rate 1/6
-    params.omega_2 = par_params(5);
-    params.phi = par_params(6); %percentage of microtubules in posterior direction for biased angle distn [parton et al 2011]
-    params.x_0= par_params(7);  %Initially in first compartment, ie. at NPC
-    params.Lx = 52; %length of cell in x direction
-    params.Ly = 37; %in y direction
-    params.nuc_radius = 10; %radius of nucleus
-    params.theta_0 = par_params(8); %initial angle is 0
+if option_a_summary_statistic %choose which type of summary statistic to use
     
-else
-    params = par_params;
-end
-
-L=params.Lx;
-time_vec = (0.05:0.05:0.15)*0.5;
-t=time_vec*60^2;
-l_t = length(time_vec);
-jumps = zeros(N,10^3);
-q_estimate = zeros(L/delx+1,l_t);
-xpos = zeros(N,10^3);
-xpos_discrete_time = zeros(N,10^3);
-
-for j=1:N
-    [~, ~, ~, ~, xpos_temp, ~, jump_temp] = velocityjump2D_with_nucleus(max(time_vec), params, 1, 2, 0);
-    jumps(j,1:length(jump_temp)) = jump_temp;
-    xpos(j,1:length(xpos_temp)) = xpos_temp;
-    for w=1:l_t
-        if isempty(xpos(j,find(t(w)<jumps(j,:),1,'first')))
-            xpos_discrete_time(j,w) = L;
-        else
-            xpos_discrete_time(j,w) =  min(xpos(j,find(t(w)<jumps(j,:),1,'first')),L);
+    %runs velocityjump2D_ModesInput which is a velocity jump process for a single particle
+    %runs this many times for many particles and returns simulated estimates of
+    %mfpt
+    
+    if is_parallel
+        params.nu1 = par_params(1); %speed of RNP complex under active transport [zimyanin et al 2008]
+        params.nu2 = par_params(2); %ratio between speed for active transport vs diffusion [zimyanin et al 2008]
+        params.lambda_1=0;   %1/0.13; %transition rate =7.69 [zimyanin et al 2008]
+        params.lambda_2 = par_params(3);
+        params.omega_1= par_params(4);    %1/6*(num_modes>1); %rate of falling off the microtubule [zimyanin et al 2008] since average track length 2.4 - 2.8 microns -> average jump for 6s -> rate 1/6
+        params.omega_2 = par_params(5);
+        params.phi = par_params(6); %percentage of microtubules in posterior direction for biased angle distn [parton et al 2011]
+        params.x_0= par_params(7);  %Initially in first compartment, ie. at NPC
+        params.Lx = 52; %length of cell in x direction
+        params.Ly = 37; %in y direction
+        params.nuc_radius = 10; %radius of nucleus
+        params.theta_0 = par_params(8); %initial angle is 0
+        
+    else
+        params = par_params;
+    end
+    non_infinite = 0;
+    t_max = 1;  %if this is too small, some runs will not reach anchoring giving infinite mfpt _> rejection.
+    % these are probably not an issue as they would give large mfpt anyway,
+    % which would be rejected. But can increase this.
+    
+    anchor_times = zeros(num_particles,1);
+    num_jumps = zeros(num_particles,1);
+    jump_distances = zeros(num_particles,1);
+    parfor j=1:num_particles
+        [~, anchor_times(j), ~, ~, pathx, pathy, ~] = velocityjump2D_with_nucleus(t_max, params, 1, 2, 0);;
+        num_jumps(j) = length(pathx);
+        jump_distances(j) = median(sqrt(diff(pathx).^2+diff(pathy).^2)); %median(abs(diff(pathx)))
+    end
+    mean_fp_time = mean(anchor_times);  % use mean first passge time as one summary statistic
+    mean_num_jumps = mean(num_jumps); %use mean number of jumps in a single passage as another
+    mean_jump_distances = mean(jump_distances); %use mean jump distance as final summary statistic
+    q_estimate = [mean_fp_time; mean_num_jumps; mean_jump_distances];
+    %non_infinite = 0;
+    % if non_infinite
+    % while isinf(mean_fp_time)
+    %     t_max = 2*t_max;
+    %     parfor j=1:num_particles
+    %     [~, anchor_times(j), ~, ~, pathx, ~] = velocityjump2D_ModesInput(t_max, params, 1, 0, 1, 0, r_random*j);
+    %     num_jumps(j) = length(pathx);
+    %     end
+    %     mean_fp_time = mean(anchor_times);
+    %     mean_num_jumps = mean(num_jumps);
+    % end
+    % end
+else %else use the spatial distribution and kl divergence
+    
+    %runs velocityjump2D_ModesInput which is a velocity jump process for a single particle
+    %runs this many times for many particles and returns simulated estimates of
+    %mfpt
+    
+    delx = 1; %bin size;
+    if is_parallel
+        params.nu1 = par_params(1); %speed of RNP complex under active transport [zimyanin et al 2008]
+        params.nu2 = par_params(2); %ratio between speed for active transport vs diffusion [zimyanin et al 2008]
+        params.lambda_1=0;   %1/0.13; %transition rate =7.69 [zimyanin et al 2008]
+        params.lambda_2 = par_params(3);
+        params.omega_1= par_params(4);    %1/6*(num_modes>1); %rate of falling off the microtubule [zimyanin et al 2008] since average track length 2.4 - 2.8 microns -> average jump for 6s -> rate 1/6
+        params.omega_2 = par_params(5);
+        params.phi = par_params(6); %percentage of microtubules in posterior direction for biased angle distn [parton et al 2011]
+        params.x_0= par_params(7);  %Initially in first compartment, ie. at NPC
+        params.Lx = 52; %length of cell in x direction
+        params.Ly = 37; %in y direction
+        params.nuc_radius = 10; %radius of nucleus
+        params.theta_0 = par_params(8); %initial angle is 0
+        
+    else
+        params = par_params;
+    end
+    
+    L=params.Lx;
+    time_vec = (0.05:0.05:0.15)*0.5;
+    t=time_vec*60^2;
+    l_t = length(time_vec);
+    jumps = zeros(N,10^3);
+    q_estimate = zeros(L/delx+1,l_t);
+    xpos = zeros(N,10^3);
+    xpos_discrete_time = zeros(N,10^3);
+    
+    for j=1:N
+        [~, ~, ~, ~, xpos_temp, ~, jump_temp] = velocityjump2D_with_nucleus(max(time_vec), params, 1, 2, 0);
+        jumps(j,1:length(jump_temp)) = jump_temp;
+        xpos(j,1:length(xpos_temp)) = xpos_temp;
+        for w=1:l_t
+            if isempty(xpos(j,find(t(w)<jumps(j,:),1,'first')))
+                xpos_discrete_time(j,w) = L;
+            else
+                xpos_discrete_time(j,w) =  min(xpos(j,find(t(w)<jumps(j,:),1,'first')),L);
+            end
         end
     end
-end
-for w=1:l_t
-    %split into bins
-    [Num_in_bins,~] = histc(xpos_discrete_time(:,w),0:delx:L);
-    if sum(Num_in_bins) ~= N
-        xpos_discrete_time(:,w)
-        ename = sprintf('ABC_errors.txt');
-        fileIDerror = fopen(ename,'w');
-        fprintf(fileIDerror,'%f \n',xpos_discrete_time);
-        fclose('all');
-        error('outside of 0:L');
+    for w=1:l_t
+        %split into bins
+        [Num_in_bins,~] = histc(xpos_discrete_time(:,w),0:delx:L);
+        if sum(Num_in_bins) ~= N
+            xpos_discrete_time(:,w)
+            ename = sprintf('ABC_errors.txt');
+            fileIDerror = fopen(ename,'w');
+            fprintf(fileIDerror,'%f \n',xpos_discrete_time);
+            fclose('all');
+            error('outside of 0:L');
+        end
+        q_estimate(:,w) = Num_in_bins/N/delx; %estimate of q at time T
     end
-    q_estimate(:,w) = Num_in_bins/N/delx; %estimate of q at time T
 end
+
 end
-function dist = distance_metric(q1,q2,L)
-%equal weightings to each of the time points currently
-delx = 1;
-dist = 0;
-for j=1:length(q1(1,:))
-    dist = dist + kldiv((0:delx:L)',q1(:,j)+eps,q2(:,j)+eps);
+
+function dist = distance_metric(q1,q2,L, option_a_distance)
+if option_a_distance %euclidian
+    %NB currently no scaling between different statistics
+    scaling = [400;500;0.5];
+    dist = sum(((q1-q2)./scaling).^2);
+    
+else %kl divergence
+    %equal weightings to each of the time points currently
+    delx = 1;
+    dist = 0;
+    for j=1:length(q1(1,:))
+        dist = dist + kldiv((0:delx:L)',q1(:,j)+eps,q2(:,j)+eps);
+    end
 end
 end
--- compare_posterior_qualities.m
+++ compare_posterior_qualities.m
@@ -2,18 +2,24 @@ function [entropy, quality, proportion_in_interval] = compare_posterior_qualitie
 %compare quality of posterior from ABC with ABC_APMC and ABC_weights
 %dependent on distance
 
-% prior_params = [1.16, 0.8, 0.11, 0.42, 0.84, 0.58, 0.5, 0];
-% real_params = prior_params; %if real params used are different then MUST change this!
-% real_params(1) = 1;
-% range = 0.8;
-% indices = [1,4,6];
+prior_params = [1.16, 0.8, 0.11, 0.42, 0.84, 0.58, 0.5, 0];
+real_params = prior_params; %if real params used are different then MUST change this!
+real_params(1) = 1;
+range = 0.8;
+indices = [1,4,6];
+alpha = 0.5;
 
 proportion_in_interval = 0;
 entropy = 0;
 quality = 0;
 %num_runs = 20;
+
+% abc_theta = zeros(N*num_runs*alpha,3);
+% abc_weights = zeros(N*num_runs*alpha,1);
 parfor b=1:num_runs
-    [~,~,entropy_temp,quality_temp,contained_in_pred_interval] =ABC_Weights_depend_on_dist(N,b);
+    [abc_theta_temp,abc_weights_temp,entropy_temp,quality_temp,contained_in_pred_interval] =ABC_Weights_depend_on_dist(N,b);
+%     abc_theta((1+(b-1)*N*alpha):(N*b*alpha),:) = abc_theta_temp;
+%     abc_weights((1+(b-1)*N*alpha):(N*b*alpha),:) = abc_weights_temp;    
     proportion_in_interval = proportion_in_interval+contained_in_pred_interval;
     entropy = entropy+entropy_temp;
     quality = quality+quality_temp;
@@ -22,5 +28,7 @@ end
 proportion_in_interval = proportion_in_interval/num_runs;
 entropy = entropy/num_runs;
 quality = quality/num_runs;
+
+%[posterior,quality_of_posterior] = multi_dim_bin_posterior(abc_theta,abc_weights,prior_params,real_params,range,indices,1);
 
 end
